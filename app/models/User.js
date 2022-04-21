31e6850099598d16f1abbe88ec4091a9c1fe7e55
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-21 13:42:04 -0600

    agregando prueba 2 fecha

--- app/models/User.js
+++ app/models/User.js
@@ -1,10 +1,10 @@
 class User {
-    constructor(id,username,name,bio,dateCreated,lastUpdated){
+    constructor(id,username,name,bio){
         this.id = id,
         this.username = username,
         this.bio = bio,
-        this.dateCreated = dateCreated,
-        this.lastUpdated = lastUpdated
+        this.dateCreated = new Date()
+        this.lastUpdated = new Date()
     }
 }
 
--- test/models/User.test.js
+++ test/models/User.test.js
@@ -2,7 +2,12 @@ const User = require('./../../app/models/User')
 
 describe('Test para User', () => {
     test('Creación de user', () => {
-        const user = new User(12,'UlisesOrnelas','UlisO','Bio','dataCreated','lastUpdated')
+        const user = new User(12,'UlisesOrnelas','@UlisO','Bio')
         expect(user.id).toBe(12)
+    })
+    test('Fechas en atributo de User', () => {
+        const user = new User(54,'Erika Machain','@erimach05','Bio')
+        expect(user.dateCreated).not.toBeUndefined()
+        expect(user.dateCreated).not.toBeUndefined()
     })
 })
