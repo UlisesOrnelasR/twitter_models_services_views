b65b263de491fbf446511bf978bb9465d1003d31
UlisesOrnelasR <ornelas.ulises19@gmail.com>
2022-04-22 17:17:36 -0600

    agregando descripción a Prueba 1

--- test/models/UserView.test.js
+++ test/models/UserView.test.js
@@ -5,8 +5,8 @@ const UserView = require('./../../app/models/UserView')
 describe('Pruebas para UserView',() => {
 
 
-    test('', () => {
-        const payload = null
+    test('Requerimiento 1: damos valor de "null" en el payload y devuelve "error"',() => {
+        const payload = null //asignando valor null a payload
         const result = UserView.createUser(payload)
         // https://jestjs.io/docs/using-matchers#strings
         expect(result.error).toMatch(/payload no existe/)
